a3db971f914c40fccce8e84bdaca8907ba63b637
Younesse AL AMRI <younes671@live.fr>
2024-02-05 13:38:03 +0100

    commit rajout foreach

--- Exo_2/js/main.js
+++ Exo_2/js/main.js
@@ -1,19 +1,25 @@
-const block = document.querySelector(".block")
-const carre = document.querySelector(".carre")
 
+const carres = document.querySelectorAll(".carre");
 
-block.addEventListener("click", function(event){
-    const targetElement = event.target
-    
-    // console.log(targetElement)
-    if(targetElement.classList.contains("carre")){
-        const btn = targetElement.closest(".carre")
-        btn.style.backgroundColor = '#FF0000'
-        btn.style.color = "#FFFFFF"
-        btn.style.transform = 'scale(0.9) rotate (360deg)'
-    } 
-    
+carres.forEach((carre) =>{
+    carre.addEventListener("click", function(){
+        carre.classList.toggle("active");
+    })
 })
+
+
+// block.addEventListener("click", function(event){
+//     const targetElement = event.target
+    
+//     // console.log(targetElement)
+//     if(targetElement.classList.contains("carre")){
+//         const btn = targetElement.closest(".carre")
+//         btn.style.backgroundColor = '#FF0000'
+//         btn.style.color = "#FFFFFF"
+//         btn.style.transform = 'scale(0.9) rotate(360deg)'
+//     } 
+    
+// })
 
 
 
